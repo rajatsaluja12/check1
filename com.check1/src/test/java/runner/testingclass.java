545f856c936859abbf5fa700a7b21fa3f9a7a895
rajat.saluja12 <er.rajatsaluja@gmail.com>
2018-04-22 12:16:17 +0530

    commit true

--- com.check1/src/test/java/runner/testingclass.java
+++ com.check1/src/test/java/runner/testingclass.java
@@ -5,7 +5,7 @@ import org.testng.annotations.Test;
 public class testingclass {
 	@Test
 	public static void methodnew() {
-		System.out.println("rajat is pagal");
+		System.out.println("Rajat has recieved good offer from Capgemini company");
 
 	}
 
