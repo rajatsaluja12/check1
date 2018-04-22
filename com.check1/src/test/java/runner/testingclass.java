68e7ff525cebffda3690def5109e798a2de7e2bf
rajat.saluja12 <er.rajatsaluja@gmail.com>
2018-04-22 14:03:02 +0530

    checking done

--- com.check1/src/test/java/runner/testingclass.java
+++ com.check1/src/test/java/runner/testingclass.java
@@ -6,7 +6,7 @@ public class testingclass {
 	@Test
 	public static void methodnew() {
 		System.out.println("Almighty God help me seek the offer from good company");
-		System.out.println("Absolutely True");
+		//System.out.println("Absolutely True");
 
 	}
 
