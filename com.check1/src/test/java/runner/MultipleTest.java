a18328b82a666f46bb244f6e0014be363c34714e
rajatsaluja12 <37304494+rajatsaluja12@users.noreply.github.com>
2019-03-09 10:29:57 +0530

    Dome
    Change

--- com.check1/src/test/java/runner/MultipleTest.java
+++ com.check1/src/test/java/runner/MultipleTest.java
@@ -13,7 +13,7 @@ public WebDriver driver;
 
       System.out.println("This is the Test Case number One");
       System.out.println("Changes done");
-      System.out.println("Changes reverted");
+      //System.out.println("Changes reverted");
 
   }
 
